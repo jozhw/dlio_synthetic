50a1bc00ec4d9d56469ed815f2d68a5239ba5001
John W. <112655410+jozhw@users.noreply.github.com>
2024-06-27 11:23:05 -0500

    add: plots for imagenet 30,000 En for red channels

--- graphing/scatterplots.py
+++ graphing/scatterplots.py
@@ -218,7 +218,7 @@ class EntropyCompressionRatioPlots:
             x,
             y,
             label="Theoretical (8/En)",
-            color="green",
+            color="black",
             linestyle="dashed",
         )
         plt.title(ptitle)
@@ -236,32 +236,47 @@ class EntropyCompressionRatioPlots:
         plt.show()
 
     @staticmethod
-    def graph_multidata_entropy_and_compression_ratio(
-        original_data: str,
-        d1: str,
-        d1_name: str,
-        d2: str,
-        d2_name: str,
+    def graph_multi_comparison_entropy_and_compression_ratio(
+        func,
+        data1: str,
+        source1: str,
+        label1: str,
+        entropy_label: str,
+        label_color: str,
+        data2: str,
+        source2: str,
+        label2: str,
+        entropy_label2: str,
+        label_color2: str,
+        data3: str,
+        source3: str,
+        label3: str,
+        entropy_label3: str,
+        label_color3: str,
         compression_type: str,
-        source: str,
     ):
         # if it is a synthetic image, then the naming of the file should
         # have synthetic in it
 
-        df = pd.read_csv(original_data)
-        df1 = pd.read_csv(d1)
-        df2 = pd.read_csv(d2)
-        num_rows = df.shape[0]
+        x = np.array(np.arange(0.1, 8, 0.1))
+        y = func(x)
 
-        fname = "{}-multidata-entropy-and-{}-compression-ratio-plot.png".format(
-            num_rows, compression_type
+        df = pd.read_csv(data1)
+        df2 = pd.read_csv(data2)
+        df3 = pd.read_csv(data3)
+        num_rows = df.shape[0] + df2.shape[0] + df3.shape[0]
+
+        fname = "{}-{}-and-{}-and{}-imgs-entropy-and-{}-compression-ratio-plot".format(
+            num_rows, source1, source2, source3, compression_type
         )
-        ptitle = "Multidata Entropy and Compression Ratio for {} Images of Each Dataset".format(
-            num_rows, compression_type
+        ptitle = (
+            "{} and {} and {} Image Entropy and Compression Ratio for {} Images".format(
+                source1, source2, source3, num_rows, compression_type
+            )
         )
 
         save_fname = FileNamingTool.generate_filename(
-            "./results/plots", fname, "png", source
+            "./results/plots", fname, "png", source1
         )
 
         # get the specific name of the compression_ratio, which the data
@@ -271,26 +286,61 @@ class EntropyCompressionRatioPlots:
 
         # the o prefix indicates original and the s indicates synthetic
 
-        ocratio = df[cr_name]
-        cratio1 = df1[cr_name]
+        cratio = df[cr_name]
         cratio2 = df2[cr_name]
+        cratio3 = df3[cr_name]
 
-        oentropy = df["entropy"]
-        entropy1 = df1["entropy"]
-        entropy2 = df2["entropy"]
+        entropy = df[entropy_label]
+        entropy2 = df2[entropy_label2]
+        entropy3 = df3[entropy_label3]
 
-        plt.figure(figsize=(12, 8))
+        plt.figure(figsize=(16, 9))
         # graph original images data
-        plt.scatter(oentropy, ocratio, color="blue", alpha=0.5, label="Original Images")
+        plt.scatter(
+            entropy,
+            cratio,
+            color=label_color,
+            s=30,
+            marker=".",
+            alpha=0.3,
+            label=label1 + " {} Images".format(df.shape[0]),
+        )
         # graph synthetic images data
-        plt.scatter(entropy1, cratio1, color="red", alpha=0.5, label=d1_name)
+        plt.scatter(
+            entropy2,
+            cratio2,
+            color=label_color2,
+            marker=".",
+            s=30,
+            alpha=0.3,
+            label=label2 + " {} Images".format(df2.shape[0]),
+        )
 
-        plt.scatter(entropy2, cratio2, color="green", alpha=0.5, label=d2_name)
+        # graph synthetic images data
+        plt.scatter(
+            entropy3,
+            cratio3,
+            s=30,
+            marker=".",
+            color=label_color3,
+            alpha=0.3,
+            label=label3 + " {} Images".format(df3.shape[0]),
+        )
+        plt.plot(
+            x,
+            y,
+            label="Theoretical (8/En)",
+            color="black",
+            linestyle="dashed",
+        )
         plt.title(ptitle)
         plt.xlabel("Entropy")
         plt.ylabel("Compression Ratio")
-        plt.legend(loc="upper left")
-        # plt.ylim([0, 20])
+
+        plt.xlim([0, 8])
+        plt.ylim([0, 20])
+
+        plt.legend(loc="upper right")
         plt.grid(True)
 
         plt.savefig(save_fname)
@@ -418,25 +468,23 @@ if __name__ == "__main__":
     comparison_paths = [
         "./results/20240605T123902==2=localcatsanddogs--results-cats-and-dogs-12430.csv",
         "./results/20240430T121325==1=eagleimagenet--results-imagenet-rand-300000.csv",
-        "./results/20240626T182026==2k--12430-e1-redchannel-processed-images-results.csv",
-        "./results/20240626T184532==2l--12430-e2-redchannel-processed-images-results.csv",
-        "./results/20240620T211256==2i--12430-e2-processed-images-results.csv",
-        "./results/20240626T195821==2m--12430-e3-redchannel-processed-images-results.csv",
-        "./results/20240621T010116==2j--12430-e3-processed-images-results.csv",
+        "./results/20240626T195539==3a--30000-e1-redchannel-processed-images-results.csv",
+        "./results/20240626T021406==3b--30000-e2-redchannel-processed-images-results.csv",
+        "./results/20240626T023210==3c--30000-e3-redchannel-processed-images-results.csv",
     ]
 
     EntropyCompressionRatioPlots.graph_data_comparison_entropy_and_compression_ratio(
         EntropyCompressionRatioPlots.theoretical_maximumCr,
-        comparison_paths[5],
-        "Local Cats and Dogs (E3)",
-        "E3_red",
-        "E3_red",
-        "red",
-        comparison_paths[6],
-        "Local Cats and Dogs (E3)",
-        "E3",
-        "E3",
+        comparison_paths[3],
+        "Imagenet (E2)",
+        "E2_red",
+        "E2_red",
         "blue",
+        comparison_paths[4],
+        "Imagenet (E3)",
+        "E3_red",
+        "E3_red",
+        "green",
         "npz",
     )
 
