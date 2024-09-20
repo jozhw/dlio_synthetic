efc5ea4691af5de06442b73f0de0874dd96eba18
John Wu <j_zhw@proton.me>
2024-09-20 06:21:57 +0000

    add: all imagenet ilsvrc paths

--- scripts/collect_imagenet_paths.sh
+++ scripts/collect_imagenet_paths.sh
@@ -2,12 +2,15 @@
 
 original_dir="$PWD"
 
-TARGET_DIR="${1:-$HOME/eagle/datasets/ImageNet/ILSVRC/Data/CLS-LOC/train}"
+TARGET_DIR="${1:-/lus/eagle/projects/datasets/ImageNet/ILSVRC/Data}"
 
 if [[ ! -d "$TARGET_DIR" ]]; then
     echo "Directory $TARGET_DIR does not exist."
     exit 1
 fi
+
+# Convert TARGET_DIR to absolute path
+TARGET_DIR=$(readlink -f "$TARGET_DIR")
 
 current_path=$(pwd)
 echo "Current Path: $current_path"
@@ -15,11 +18,14 @@ echo "Current Path: $current_path"
 # start timer
 SECONDS=0
 
-files=$(find "$TARGET_DIR" -type f)
+# Change to the target directory
+cd "$TARGET_DIR"
+
+files=$(find "$TARGET_DIR" -type f \( -name '*.jpg' -o -name '*.jpeg' -o -name '*.JPEG' -o -name '*.JPG' \))
 
 # date for csv
 date=$(date +'%Y-%m-%d')
-csv_file="imagenet_paths_${date}.csv"
+csv_file="imagenet_ilsvrc_paths.csv"
 
 # switch to where paths will be stored
 output_dir="$original_dir/assets"
@@ -30,9 +36,11 @@ echo "path" > "$output_dir/$csv_file"
 
 while IFS= read -r file; do
     echo "\"$file\"" >> "$output_dir/$csv_file"
-done <<< "$image_files"
+done <<< "$files"
+
+# Change back to the original directory
+cd "$original_dir"
 
 elapsed_time=$SECONDS
-
 echo "CSV file created at: $output_dir/$csv_file"
 echo "Time taken: $elapsed_time seconds"
